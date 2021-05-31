db2696ed1726f6e97849b7947280b7ed0cbd5bf5
Keyu <imbladen88@gmail.com>
2021-05-31 03:01:19 +0100

    Create 404.blade.php

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,41 @@
+@extends('shop')
+
+@section('content')
+
+<section class="page-section" id="services" style="margin-bottom:-5%;">
+    <div class="container">
+        <div class="row">
+            <h1 style="margin:auto;margin-top:20%;">404 - Page not found</h1>
+        </div>
+    </div>
+</section>
+
+<footer class="footer py-4">
+    <div class="container" style="position: absolute; bottom: 0; left: 0; right: 0; margin-bottom:1%">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-left">MagicShirts © AINet - Politécnico de Leiria</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-lg-4 text-lg-right">
+                <a class="mr-3" href="#!">Privacy Policy</a>
+                <a href="#!">Terms of Use</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<!-- Contact form JS-->
+<script src="mail/jqBootstrapValidation.js"></script>
+<script src="mail/contact_me.js"></script>
+<!-- Core theme JS-->
+<script src="/js/scripts.js"></script>
+
+@stop
